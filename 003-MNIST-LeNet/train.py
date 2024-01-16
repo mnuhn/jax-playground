adde822d9e4f0d8932ed064e5a917368ede2c063
Malte Nuhn <painted.now@gmail.com>
2024-01-16 23:57:06 +0100

    Allow printing histograms of predictions

--- 003-MNIST-LeNet/train.py
+++ 003-MNIST-LeNet/train.py
@@ -84,32 +84,41 @@ class LeNet():
     self.num_classes = num_classes
     self.dense_layers = dense_layers
     self.params = []
+    self.param_names = []
     self.bias_term = bias_term
 
     # 5x5 Kernel, 1 input channel, 6 output channels.
     kernel1 = np.random.normal(size=(5,5,1,6), loc=0, scale=1.0/(25.0**0.5))
     self.params.append(kernel1)
+    self.param_names.append("kernel1")
 
     if self.bias_term:
       kernel1_bias = np.zeros(shape=6)
       self.params.append(kernel1_bias)
+      self.param_names.append("kernel1_bias")
+
 
     # 5x5 Kernel, 6 input channels, 16 output channels.
     kernel2 = np.random.normal(size=(5,5,6,16), loc=0, scale=1.0/(150.0**0.5))
     self.params.append(kernel2)
+    self.param_names.append("kernel2")
+
 
     if self.bias_term:
       kernel2_bias = np.zeros(shape=16)
       self.params.append(kernel2_bias)
+      self.param_names.append("kernel2_bias")
 
     # 1st dense layer
     layer = np.random.normal(size=(400, self.dense_layers[0]),
                              loc=0,scale=1.0/400**0.5)
     self.params.append(layer)
+    self.param_names.append("dense_1")
 
     if self.bias_term:
       layer = np.zeros(shape=self.dense_layers[0])
       self.params.append(layer)
+      self.param_names.append("dense_1_bias")
 
     # additional layers as specified via params
     for i in range(1, len(dense_layers)):
@@ -118,9 +127,11 @@ class LeNet():
                                      self.dense_layers[i]),
                                loc=0,scale=(1.0/self.dense_layers[i-1])**0.5)
       self.params.append(layer)
+      self.param_names.append(f"layer_{i}")
       if self.bias_term:
         layer = np.zeros(shape=self.dense_layers[i])
         self.params.append(layer)
+        self.param_names.append(f"layer_{i}_bias")
 
     # Place on device.
     for i in range(0, len(self.params)):
@@ -128,6 +139,7 @@ class LeNet():
 
   # Returns log softmax.
   def predict(self, params, x):
+    hists = []
     x = jnp.expand_dims(x, axis=-1) # Bx28x28x1
     x = lax.conv_general_dilated(lhs=x,
                                  rhs=params[0],
@@ -136,8 +148,10 @@ class LeNet():
                                  dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
                                  lhs_dilation=(1,1),
                                  rhs_dilation=(1,1)) # Bx28x28x6
+    hists.append(x)
     # Bias term.
     x = x + params[1]
+    hists.append(x)
 
     # Avg pooling.
     # This tensor product is needed to ensure we keep all 6 channels separate.
@@ -152,7 +166,8 @@ class LeNet():
                                  dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
                                  lhs_dilation=(1,1),
                                  rhs_dilation=(1,1)) # Bx14x14x6
-    x = nn.relu(x)
+    x = nn.sigmoid(x)
+    hists.append(x)
 
     # 2nd convolution (without padding).
     x = lax.conv_general_dilated(lhs=x,
@@ -163,6 +178,7 @@ class LeNet():
                                  lhs_dilation=(1,1),
                                  rhs_dilation=(1,1)) # Bx10x10x16
 
+    hists.append(x)
     # Bias term.
     x = x + params[3]
 
@@ -180,7 +196,6 @@ class LeNet():
                                   dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
                                   lhs_dilation=(1,1),
                                   rhs_dilation=(1,1)) # Bx5x5x16
-    x = nn.relu(x)
 
     # Flatten.
     x = x.reshape(x.shape[0], -1) # Bx400
@@ -191,21 +206,34 @@ class LeNet():
       first_dense_idx = 2
 
     for i in range(0, len(self.dense_layers)):
-      x = nn.relu(x)
+      x = nn.sigmoid(x)
+      hists.append(x)
       if self.bias_term:
         x = jnp.einsum('bi,ij',x,params[first_dense_idx+i*2])
+        hists.append(x)
         x = x + params[first_dense_idx+i*2+1]
       else:
         x = jnp.einsum('bi,ij',x,params[first_dense_idx+i])
 
     x = nn.log_softmax(x)
-    return x
+    hists.append(x)
+    return x, hists
 
   def loss(self, params, batch_images,batch_labels):
-    return - jnp.mean(batch_labels * self.predict(params,batch_images))
+    p, h = self.predict(params,batch_images)
+    r = - jnp.mean(batch_labels * p)
+    return r
 
   def test_loss(self, params):
-    return - jnp.mean(test_labels * self.predict(params,test_imgs))
+    p, h = self.predict(params,test_imgs)
+    r = - jnp.mean(test_labels * p)
+    return r, h
+
+def histo(val):
+  val = round(val)
+  val = min(max(0,val),7)
+  chars = [" ", "▁", "▂", "▃", "▄", "▅", "▆", "▇"]
+  return chars[val]
 
 def train():
   dense_layers = [int(x) for x in args.dense_layers.split(',')]
@@ -236,6 +264,8 @@ def train():
   for i in pbar:
     cur_images, cur_labels = next(batches)
     g = model.gradient(model.params,cur_images,cur_labels)
+
+
     updates, opt_state = tx.update(g, opt_state)
     model.params = optax.apply_updates(model.params, updates)
 
@@ -245,12 +275,25 @@ def train():
             f'test_loss={cur_test_loss:.3f}')
 
     if i % int(1+iterations/100) == 0:
+      print("Histograms:")
+      for p,n in zip(model.params, model.param_names):
+        hist, boundaries = np.histogram(p.reshape(-1), bins=100, range=(-1,1), density=True)
+        print(f"{n:<25}", "[" + "".join(histo(h) for h in hist) + "]")
+
       # Print kernels.
       # print(np.moveaxis(model.params[0],[0,1,2],[1,2,0]))
       cur_loss = model.loss(model.params,cur_images,cur_labels)
-      cur_test_loss = model.test_loss(model.params)
+      cur_test_loss, hists = model.test_loss(model.params)
+
+      print("activations")
+      for p,n in zip(hists, model.param_names):
+
+        hist, boundaries = np.histogram(p.reshape(-1), bins=100, range=(0,1), density=True)
+        print(f"{n:<25}", "[" + "".join(histo(h) for h in hist) + "]")
+
+
       cur_preds = np.argmax(
-              model.predict(model.params,test_imgs), axis=-1).tolist()
+              model.predict(model.params,test_imgs)[0], axis=-1).tolist()
       cur_labels = np.argmax(test_labels, axis=-1).tolist()
       eval_print.print_confusions(
               i, cur_preds, cur_labels, test_imgs.shape[0], cur_loss)
