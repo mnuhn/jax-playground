c890db78ac33623fb230d088d344e016383bc398
Malte Nuhn <painted.now@gmail.com>
2024-03-19 00:30:03 +0100

    implement some simple controllers for the cart pole problem - and add gifs showing the trajectories

--- 007-Cart-Pole/equations.py
+++ 007-Cart-Pole/equations.py
@@ -1,6 +1,7 @@
 from math import sin, cos, pi
 import tqdm
 from PIL import Image, ImageDraw
+import random
 
 # Static
 l = 3
@@ -9,10 +10,32 @@ mu_p = 2.0
 g = 9.81
 m_pole = 1.0
 m_cart = 3.0
+MAX_FORCE = 10.0
 
 
-def controller(state):
-  return 0.0  #random.random()
+def move_nothing(state):
+  return 0.0
+
+
+def move_constant(state):
+  return 3.0
+
+
+def move_random(state):
+  return 10 * (random.random() - 0.5)
+
+
+def move_opposite(state):
+  # If the pole falls to the right, move right - and vice versa.
+  angle = (state.theta - pi)
+  return - angle * 10
+
+def move_opposite_upswing(state):
+  # If the pole falls to the right, move right - and vice versa.
+  angle = (state.theta - pi)
+  if abs(angle) > pi * 0.5:
+    return - angle * 10
+  return angle * 10
 
 
 class PoleCartState:
@@ -54,7 +77,7 @@ def time_step(state, force, eps=0.0001):
   return PoleCartState(x=x, v=v, theta=theta, theta_dot=theta_d)
 
 
-def draw(frame, state):
+def draw(frame, state, force):
   x_end = state.x + l * sin(state.theta)
   y_end = -l * cos(state.theta)
 
@@ -69,27 +92,58 @@ def draw(frame, state):
 
   draw.line((xx(state.x - 1), yy(0), xx(state.x + 1), yy(0)),
             fill=(255, 255, 255, 128))
+
+  draw.line((xx(state.x), yy(1), xx(state.x + force), yy(1)),
+            fill=(255, 0, 0, 128))
+
   draw.line((xx(x_end), yy(y_end), xx(state.x), yy(0)),
             fill=(255, 255, 255, 128))
 
   return im
 
 
-frame = 0
-images = []
+def evaluate(start_state, controller, image_fn=None):
+  frame = 0
+  images = []
+  state = start_state
+  steps_up = 0
+  while True:
+    frame += 1
 
-state = PoleCartState(x=0.0, v=0.0, theta=0.01 * pi, theta_dot=0.0)
+    force = controller(state)
+    force = max(min(force, MAX_FORCE), -MAX_FORCE)
 
-while True:
-  frame += 1
-  state = time_step(state, force=controller(state))
-  if frame % 2500 == 0:
-    images.append(draw(frame, state))
-  if frame > 300000:
-    break
+    state = time_step(state, force=force)
+    if state.theta < 0.1 * pi or state.theta > 1.9 * pi:
+      steps_up += 1
+    if frame % 2500 == 0 and image_fn:
+      images.append(draw(frame, state, force))
+    if frame > 300000:
+      break
 
-images[0].save("frames.gif",
-               save_all=True,
-               append_images=images[1:],
-               duration=10,
-               loop=0)
+  if image_fn:
+    images[0].save(image_fn,
+                   save_all=True,
+                   append_images=images[1:],
+                   duration=10,
+                   loop=0)
+
+  return steps_up
+
+
+start_state = PoleCartState(x=0.0, v=0.0, theta=0.01 * pi, theta_dot=0.0)
+
+evaluate(start_state, controller=move_nothing, image_fn="move_nothing.gif")
+evaluate(start_state, controller=move_constant, image_fn="move_constant.gif")
+evaluate(start_state, controller=move_random, image_fn="move_random.gif")
+evaluate(start_state, controller=move_opposite, image_fn="move_opposite.gif")
+evaluate(start_state, controller=move_opposite, image_fn="move_opposite_upswing.gif")
+
+
+start_state = PoleCartState(x=0.0, v=0.0, theta=0.01 * pi, theta_dot=0.5)
+
+evaluate(start_state, controller=move_nothing, image_fn="move_nothing2.gif")
+evaluate(start_state, controller=move_constant, image_fn="move_constant2.gif")
+evaluate(start_state, controller=move_random, image_fn="move_random2.gif")
+evaluate(start_state, controller=move_opposite, image_fn="move_opposite2.gif")
+evaluate(start_state, controller=move_opposite, image_fn="move_opposite_upswing2.gif")
